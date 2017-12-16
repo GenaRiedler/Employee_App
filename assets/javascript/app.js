5fa84ad3d725a55da3dd16662438cc1bc6db71fa
Mateo Vargas <mateovargas@mateos-mbp.dynamic.ucsd.edu>
2017-12-16 12:36:32 -0800

    Added comments on app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
-//  START CODING BELOW!!
-// Initialize Firebase
+//Code to initialize Firebase. This is Mateo's personal firebase config.
 var config = {
 
   apiKey: "AIzaSyAZf8vAIUEY1lO2MJig6tGLJQeYlcbppqA",
@@ -11,33 +10,35 @@ var config = {
 
 };
 
+//Initializes Firebase using the configuration from Mateo
 firebase.initializeApp(config);
 
+//Initialized variables for use through
 var database = firebase.database();
-var initial_name = 'Mateo Vargas';
-var initial_role = 'Software Developer';
-var initial_date = "";
-var initial_rate = 100;
+var current_name = "";
+var current_role = "";
+var start_date = "";
+var current_rate = "";
 
-var current_name = initial_name;
-var current_role = initial_role;
-var start_date = initial_date;
-var current_rate = initial_rate;
-
+//Gives each employee row and unique id.
 var employee_count = -1;
 
+//Event listener that runs function upon clicking submit.
 $('#add-emp').on('click', function(){
+
+  //Prevents the page from reloading.
   event.preventDefault();
 
-  // YOUR TASK!!!
-
-  // Code in the logic for storing and retrieving the most recent user.
+  //Gets the inputted values for the employees from the form and increments
+  //employee count.
   current_name = $('#name-input').val();
   current_role = $('#role-input').val();
   start_date = $('#date-input').val();
   current_rate = $('#rate-input').val();
   employee_count++;
 
+  //Pushes the individual entry to the database. Push adds it as one 
+  //item with a single unique id.
   database.ref().push({
 
     name: current_name,
@@ -49,42 +50,53 @@ $('#add-emp').on('click', function(){
   });
 });
 
+//Displays the new employee upon being added. Takes only a snapshot of the 
+//added child.
 database.ref().on('child_added', function(child_snapshot){
 
+  //Gets the snapshot values.
   current_name = child_snapshot.val().name;
   current_role = child_snapshot.val().role;
   start_date = child_snapshot.val().start_date;
   current_rate = child_snapshot.val().rate;
 
+  //Creates the table row
   var table_row = $('<tr>');
   table_row.attr('id', employee_count);
 
+  //Creates the name column.
   var name_col = $('<td>').text(current_name);
   name_col.attr('id', 'name');
   table_row.append(name_col);
 
+  //Creates the role column.
   var role_col = $('<td>').text(current_role);
   role_col.attr('id', 'role');
   table_row.append(role_col);
 
+  //Creates the start date column.
   var start_date_col = $('<td>').text(start_date);
   start_date_col.attr('id', 'start date');
   table_row.append(start_date_col);
 
+  //Creates the months worked column.
   var months_worked = Date.now() - start_date;
   var months_worked_col = $('<td>').text(months_worked);
   months_worked_col.attr('id', 'months worked');
   table_row.append(months_worked_col);
 
+  //Creates the Monthy Rate column.
   var rate_col = $('<td>').text(current_rate);
   rate_col.attr('id', 'rate');
   table_row.append(rate_col);
 
+  //Creates the total billed column.
   var total_billed = current_rate * months_worked;
   var total_billed_col = $('<td>').text(total_billed);
   total_billed_col.attr('id', 'total billed');
   table_row.append(total_billed_col);
 
+  //Appends entire row to the table.
   $('#emp-table').append(table_row);
 
 }, function(errorObject){
